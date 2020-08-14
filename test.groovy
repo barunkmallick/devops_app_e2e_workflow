5368560bdf50af78996499067ecf3e2a60f1c02c
barunkmallick <69379698+barunkmallick@users.noreply.github.com>
2020-08-15 03:21:31 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,1 +1,41 @@
+job('Freestyle job'){
+	
+    concurrentBuild(true)
+    
+    logRotator {
+        numToKeep(3)
+        daysToKeep(3)
+    }
+     parameters {
+        choiceParam('OPTION', ['option 1 (default)', 'option 2', 'option 3'])
+        stringParam('MY STRING PARAM', defaultValue = null, description = 'This is a dsl script generated string param')
+    }
+    scm {
+        git {
+            remote {
+                url('https://github.com/barunkmallick/devops_app_e2e_workflow.git')
+		credentials('test')    
+                   }
+		extensions {
+                cleanAfterCheckout()
+                            }
+            branch('master')
+		triggers {
+        	scm('5 * * * *')
+    		}
+        }
+    }
+   wrappers {
+	   
+        buildName('JenkinsMaven#${BUILD_NUMBER}')
+	   
+    }
+	steps {
+
+        shell ('echo ${JOB_NAME}>log.txt')
+	shell ('echo "Hello World!">>log.txt')
+    	  
+    }
+    
+}
 
