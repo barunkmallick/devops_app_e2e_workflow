c475dcb3080b0b7db1f824485bc6375abc052713
barunkmallick <69379698+barunkmallick@users.noreply.github.com>
2020-08-15 02:26:16 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -27,7 +27,7 @@ job('Freestyle job'){
     }
    wrappers {
 	   
-        buildName("${GERRIT_CHANGE_SUBJECT}"@'JenkinsMaven#${BUILD_NUMBER}')
+        buildName("JenkinsMaven#${BUILD_NUMBER}")
 	buildDescription("Executed @ ${NODE_NAME}")
 	   
     }
