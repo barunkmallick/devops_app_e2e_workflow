f5ff03bf8c792b3c7224a69ff2cc1e90b4802d40
barunkmallick <69379698+barunkmallick@users.noreply.github.com>
2020-08-15 07:35:52 +0530

    Update MultipleEnv.groovy

--- MultipleEnv.groovy
+++ MultipleEnv.groovy
@@ -16,21 +16,29 @@ freeStyleJob(jobName) {
         daysToKeep(3)
     }
     disabled(disableProj[projNames])
-    
+    label('x86 && ubuntu')
     parameters {
        stringParam('env', '','emv name:')
        booleanParam('RUN_TESTS', true, 'uncheck to disable tests')
     }
+    
      
     concurrentBuild(true)
-       
-    scm {
+      
+    multiscm {
         git {
             remote {
                 url('https://github.com/barunkmallick/devops_app_e2e_workflow.git')
 		        credentials('test')    
              }
             branch('master')
+        }
+        git {
+            remote {
+                url('https://github.com/barunkmallick/devops_app_e2e_workflow.git')
+		        credentials('test')    
+             }
+            branch('master/indiaSCM')
         }
      } 
 }
